d5a939929663dfa279328c6994aaaf479aaba267
sandersdrain <sandersdrain@outlook.com>
2024-04-19 17:25:40 +0400

    Day 1 of 100

---
+++ day-1-variables/day-1-printing.py
@@ -0,0 +1,20 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(num1 * num2)
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+print("Hello" + " " + "Rain")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# input
+print("Hello " + input("Enter your name: "))
+
+name = input()
+print(len(name))
---
+++ day-1-variables/day-1-variables.py
@@ -0,0 +1,29 @@
+name = input("Enter your name: ")
+print(name)
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+name = input("Enter your name: ")
+length = len(name)
+print(length)
+
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+user_name = input("Enter your name: ")
