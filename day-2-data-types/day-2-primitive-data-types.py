f1d3de3a17fa2a74d4208ceb1500415e6055d2ab
sandersdrain <sandersdrain@outlook.com>
2024-04-20 13:34:59 +0400

    Day 2 of 100

---
+++ day-2-data-types/day-2-primitive-data-types.py
@@ -0,0 +1,33 @@
+# Data Types
+
+# String
+
+print("Hello"[4])
+
+print("123" + "456")
+
+# Integer
+
+print(123 + 345)
+
+num1 = 123_456_789
+
+# Float
+
+num2 = 3.14159
+
+# Boolean
+
+check1 = True
+check2 = False
+
+# Operators
+print(3 + 5)
+print(7 - 4)
+print(3 * 2)
+print(6 / 3)
+print(2 ** 3)
+
+# PEMDAS
+
+print(3 * (3 + 3) / 3 - 3)
---
+++ day-2-data-types/day-2-type-error.py
@@ -0,0 +1,11 @@
+num_char = len(input("Enter your name: "))
+new_num_char = str(num_char)
+print(f"Your name ha {new_num_char} characters.")
+
+print(type(num_char))
+print(type(new_num_char))
+
+a = str(123)
+print(type(a))
+
+print(70 + float("100.5"))
